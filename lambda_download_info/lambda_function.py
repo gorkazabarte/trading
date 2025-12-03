888e5aa321d1d7a427c79310f8ad34685079c181
gorkazabarte <gorkazabartemoreno@gmail.com>
2025-12-03 17:38:29 +0100

    TRADING-0 Change the logic of download info lambda function

--- lambda_download_info/lambda_function.py
+++ lambda_download_info/lambda_function.py
@@ -1,47 +1,75 @@
-from csv import reader
-from datetime import datetime
-from json import dumps
+from calendar import monthrange
+from datetime import date
 from os import environ
+from pandas import DataFrame
 
 from boto3 import client
-from requests import Session
+from requests import get
 
-API_KEY = environ.get('API_KEY')
-CSV_URL = f'https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey={API_KEY}'
+HEADERS = {"User-Agent": "Mozilla/5.0"}
+ROWS_PER_PAGE = 1000
 S3_BUCKET: str = environ.get("S3_BUCKET")
 
 s3_client = client("s3")
+today = date.today()
+
+year = today.year + (1 if today.month == 12 else 0)
+month = today.month + 1 if today.month < 12 else 1
+days_in_next_month = monthrange(today.year + 1, 1)[1] if today.month == 12 else monthrange(today.year + 1, 1)[1]
 
 def lambda_handler(event, context):
 
+    data = []
+    start = 0
+
     try:
-        today_str = datetime.today().strftime('%Y-%m-%d')
 
-        with Session() as s:
-            download = s.get(CSV_URL)
-            decoded_content = download.content.decode('utf-8')
-            cr = reader(decoded_content.splitlines(), delimiter=',')
-            data_list = list(cr)
+        for day in range(1, days_in_next_month + 1):
 
-        headers = data_list[0]
-        rows = data_list[1:]
-        json_data = [dict(zip(headers, row)) for row in rows]
+            if (current_day := date(year, month, day)).weekday() >= 5:
+                print(f"Skipping weekend: {current_day}")
+                continue
 
-        today_data = [row for row in json_data if row.get('reportDate') == today_str]
-        s3_client.put_object(
-            Bucket=S3_BUCKET,
-            Key=f'earnings_{today_str}.json',
-            Body=dumps(today_data, ensure_ascii=False, indent=2),
-            ContentType="application/json"
-        )
+            params = {"date": "", "type": "earning", "limit": ROWS_PER_PAGE, "offset": start}
+
+            response = get(f"https://api.nasdaq.com/api/calendar/earnings?date={year}-{month:02}-{day:02}", headers=HEADERS, params=params)
+            response.raise_for_status()
+            data_json = response.json()
+
+            try:
+                rows = data_json.get("data", {}).get("rows", [])
+            except AttributeError as e:
+                print(f"Error parsing JSON for date {current_day}: {e}")
+                continue
+
+            if not rows:
+                continue
+
+            for item in rows:
+                data.append(
+                    {
+                        "Company": item.get("name"),
+                        "Date": current_day,
+                        "EPS Estimate": item.get("epsForecast"),
+                        "Market Cap": item.get("marketCap"),
+                        "Symbol": item.get("symbol"),
+                        "Time": item.get("time")
+                    }
+                )
+
+            start += ROWS_PER_PAGE
+
+        df = DataFrame(data)
+        df.to_csv("nasdaq_earnings_next_month.csv", index=False)
+        print(f"Saved {len(df)} rows")
 
         return {
             'statusCode': 200,
             'body': {
-                'records_uploaded': len(today_data),
+                'records_uploaded': len(df),
                 'message': 'Successfully uploaded earnings data',
                 's3_bucket': S3_BUCKET,
-                's3_key': f'earnings_{today_str}.json'
+                's3_key': f'{year}/{month}/companies.csv'
             }
         }
     except Exception as e:
