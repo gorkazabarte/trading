54cdc13e2095f04e290d616d1b5f67d81bb3ec38
gorkazabarte <gorkazabartemoreno@gmail.com>
2025-12-16 17:20:06 +0100

    TRADING-0 Print the event and the body

--- lambda_filter_info/lambda_function.py
+++ lambda_filter_info/lambda_function.py
@@ -89,13 +89,18 @@ def get_stock_performance(ticker: str) -> dict:
 
 
 def get_target_day(event: dict) -> tuple[int, int, int]:
+
+    print("Event received:", event)
     body = event.get("body", {})
+    print("Body extracted:", body)
 
     if isinstance(body, str):
         from json import loads
         body = loads(body)
 
-    year, month, day, _ = body["key"].split("/")
+    year = body.get("year")
+    month = body.get("month")
+    day = body.get("day")
 
     if year and month and day:
         return int(year), int(month), int(day)
