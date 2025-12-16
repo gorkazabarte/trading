5377d08498112afada1498d1fbf9cb7d816d4b42
gorkazabarte <gorkazabartemoreno@gmail.com>
2025-12-16 12:56:38 +0100

    TRADING-0 Remove download info terraform code

--- deploy/app/modules/lambda/main.tf
+++ deploy/app/modules/lambda/main.tf
@@ -7,37 +7,6 @@ locals {
   environment      = var.environment
 }
 
-resource "aws_iam_policy" "lambda_policy_download_info" {
-  name        = "${local.environment}-${local.app_name}-download-info"
-  description = "Allow Lambda to put objects in specific S3 bucket"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["s3:PutObject"]
-      Resource = "arn:aws:s3:::${local.aws_s3_bucket}/*"
-    }]
-  })
-}
-
-module "lambda_function_download_info" {
-  source         = "terraform-aws-modules/lambda/aws"
-  attach_policy  = true
-  create_package = false
-  description    = "Donwload information using web scraping"
-  environment_variables = {
-    S3_BUCKET = "${local.aws_s3_bucket}"
-  }
-  function_name  = "${local.environment}-${local.app_name}-download-info"
-  image_uri      = "${local.aws_account_id}.dkr.ecr.${local.aws_region}.amazonaws.com/${local.environment}-${local.app_name}-download-info:${local.app_version}"
-  memory_size	 = 256
-  timeout        = 180
-  package_type   = "Image"
-  policy         = aws_iam_policy.lambda_policy_download_info.arn
-  version        = "8.1.2"
-}
-
 resource "aws_iam_policy" "lambda_policy_get_calendar" {
   name        = "${local.environment}-${local.app_name}-get-calendar"
   description = "Return calendar response to API Gateway"
