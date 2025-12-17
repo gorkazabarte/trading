8067dfc338e4eda28fbb92c60b274f94185d4426
gorkazabarte <gorkazabartemoreno@gmail.com>
2025-12-17 15:04:30 +0100

    TRADING-0 Fix the outputs.tf of api module

--- deploy/app/modules/api/outputs.tf
+++ deploy/app/modules/api/outputs.tf
@@ -1,20 +1,20 @@
 output "api_gateway_url" {
   description = "The URL of the API Gateway"
-  value       = module.api_gateway.default_apigatewayv2_stage_invoke_url
+  value       = module.api_gateway.stage_invoke_url
 }
 
 output "api_gateway_id" {
   description = "The ID of the API Gateway"
-  value       = module.api_gateway.apigatewayv2_api_id
+  value       = module.api_gateway.api_id
 }
 
 output "api_gateway_arn" {
   description = "The ARN of the API Gateway"
-  value       = module.api_gateway.apigatewayv2_api_arn
+  value       = module.api_gateway.api_arn
 }
 
 output "api_gateway_execution_arn" {
   description = "The execution ARN of the API Gateway"
-  value       = module.api_gateway.apigatewayv2_api_execution_arn
+  value       = module.api_gateway.api_execution_arn
 }
 
