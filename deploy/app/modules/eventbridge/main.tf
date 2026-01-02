5b7da8ffa6ee5401b113150b72d00035709ec7c0
gorkazabarte <gorkazabartemoreno@gmail.com>
2026-01-02 20:28:47 +0100

    TRADING-0 Create EventBridge scheduler

--- deploy/app/modules/eventbridge/main.tf
+++ deploy/app/modules/eventbridge/main.tf
@@ -78,6 +78,39 @@ EOF
   }
 }
 
+resource "aws_iam_role" "eventbridge_scheduler_invoke_lambda" {
+  name = "${local.environment}-${local.app_name}-eventbridge-scheduler-invoke-lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "eventbridge_scheduler_invoke_lambda" {
+  name = "${local.environment}-${local.app_name}-eventbridge-scheduler-invoke-lambda-policy"
+  role = aws_iam_role.eventbridge_scheduler_invoke_lambda.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["lambda:InvokeFunction"],
+        Resource = local.lambda_sync_storage_arn
+      }
+    ]
+  })
+}
+
 resource "aws_scheduler_schedule" "sync_storage_daily" {
   name                = "${local.environment}-${local.app_name}-sync-storage-daily"
   description         = "Trigger Lambda sync storage once a day at 07:00 AM Madrid time"
@@ -87,7 +120,7 @@ resource "aws_scheduler_schedule" "sync_storage_daily" {
   }
   target {
     arn      = local.lambda_sync_storage_arn
-    role_arn = aws_iam_role.eventbridge_invoke_lambda.arn
+    role_arn = aws_iam_role.eventbridge_scheduler_invoke_lambda.arn
     input    = jsonencode({})
   }
   schedule_expression_timezone = "Europe/Madrid"
